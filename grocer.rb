0105f0ab6f0538ae32f9f97c309589305accd187
Israel Sanchez <israel.pro1994@gmail.com>
2019-08-05 02:16:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,49 @@
+
 def consolidate_cart(cart)
-  # code here
+  
+  newhash = {}
+
+  cart.each do |hash|
+    hash.each do |key,value|
+      if newhash[key]==nil
+        
+        newhash[key] = value
+        # first run
+        # newhash["AVACADO"] = {:price => 3.oo, :clearance => true}
+        
+        newhash[key][:count] = 1
+        # first run
+        # newhash["AVACADO"] = {:price => 3.oo, :clearance => true, :count => 1}
+      else
+        newhash[key][:count] = newhash[key][:count] + 1
+        # second run
+        # newhash["AVACADO"] = {:price => 3.oo, :clearance => true, :count => 2}
+      end
+    end
+  end
+      
+  newhash
+
+
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  
+  newhash = {}
+  
+    cart.each do |hash|
+      hash.each do |key,value| 
+        coupons.include?[key][AVACADO]
+      if coupons == 
+        coupons = 
+      
+     
+      
+        
+  
+  
+  
+  
 end
 
 def apply_clearance(cart)
@@ -13,3 +53,5 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+
